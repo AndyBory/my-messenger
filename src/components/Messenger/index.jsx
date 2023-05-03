79f6da2263337a0032ecf8ba52a15b5969abe326
AndyBory <andyborysov@gmail.com>
2023-05-03 16:02:50 +0300

    Add messenger

---
+++ src/components/Messenger/index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+class Messenger extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isReading: false,
+    };
+  }
+
+  click = () => {
+    this.setState({
+      isReading: !this.state.isReading,
+    });
+  };
+
+  render() {
+    const { user: {name, message}, } = this.props;
+    const {isReading} = this.state;
+    
+    return (
+      <article>
+        <h1>My messenger</h1>
+        <p>User name: {name}</p>
+        <p>Message: {message}</p>
+        <button disabled={isReading} onClick={this.click}>
+          {isReading ? 'Have read' : 'Read'}</button>
+      </article>
+    );
+  }
+}
+
+export default Messenger;
